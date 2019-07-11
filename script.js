0ed04573c7204d1fcde3ead4449db3fabdaa58d2
alifsyfulislam <alifsyfulislam@gmail.com>
2019-07-11 14:50:34 +0600

    v11

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var oxIcon = document.querySelector('.domestic_4');
 var camelIcon = document.querySelector('.domestic_1');
 var result = document.querySelector('#result');
 var scoreVal = document.querySelector('#score');
+var selectorDiv = document.querySelector('.selector_div');
 
 
 var slide1 = document.querySelector('.slide_1');
@@ -20,53 +21,56 @@ var caposX = 50;
 var oaposX = 0;
 var ggaposX = 0;
 
+selectorDiv.addEventListener('click', checkIT);
 
-var goatMove = setInterval(function () {
-    goatIcon.style.left = gposX+"px";
-    gposX+= 10;
-    if(gposX > width){//50
-        gposX = -100;
-    }
-},60);
+function checkIT(){
+    selectorDiv.classList.add('hidden');
+    var goatMove = setInterval(function () {
+        goatIcon.style.left = gposX+"px";
+        gposX+= 10;
+        if(gposX > width){//50
+            gposX = -100;
+        }
+    },60);
 
-var goatMove2 = setInterval(function () {
-    goatIcon2.style.left = ggaposX+"px";
-    ggaposX+= 15;
-    if(ggaposX > width){//50
-        ggaposX = -250;
-    }
-},55);
+    var goatMove2 = setInterval(function () {
+        goatIcon2.style.left = ggaposX+"px";
+        ggaposX+= 15;
+        if(ggaposX > width){//50
+            ggaposX = -250;
+        }
+    },55);
 
-var cowMove = setInterval(function () {
-    cowIcon.style.left = cposX+"px";
-    cposX+= 10;
-    if(cposX > width){//50
-        cposX = -225;
-    }
-},40);
+    var cowMove = setInterval(function () {
+        cowIcon.style.left = cposX+"px";
+        cposX+= 10;
+        if(cposX > width){//50
+            cposX = -225;
+        }
+    },40);
 
-var oxMove = setInterval(function () {
-    oxIcon.style.left = oaposX+"px";
-    oaposX+= 15;
-    if(oaposX > width){//50
-        oaposX = -1000;
-    }
-},35);
-
-
-var camelMove = setInterval(function () {
-    camelIcon.style.left = caposX+"px";
-    caposX+= 15;
-    if(caposX > width){//50
-        caposX = -500;
-    }
-},60);
+    var oxMove = setInterval(function () {
+        oxIcon.style.left = oaposX+"px";
+        oaposX+= 15;
+        if(oaposX > width){//50
+            oaposX = -1000;
+        }
+    },35);
 
 
+    var camelMove = setInterval(function () {
+        camelIcon.style.left = caposX+"px";
+        caposX+= 15;
+        if(caposX > width){//50
+            caposX = -500;
+        }
+    },60);
 
 
 
-interval = setInterval(function () {
+
+
+    interval = setInterval(function () {
         counter--;
         if (score>= 10 && counter < 0){
             clearInterval(goatMove);
@@ -93,15 +97,16 @@ interval = setInterval(function () {
         else{
             result.innerText = counter;
         }
-},1000);
+    },1000);
+    goatIcon.addEventListener('click',scoreUpdate);
+    goatIcon2.addEventListener('click',scoreUpdate1);
+    cowIcon.addEventListener('click',scoreUpdate2);
+    oxIcon.addEventListener('click',scoreUpdate3);
+    camelIcon.addEventListener('click',scoreUpdate4);
+}
 
 
 
-goatIcon.addEventListener('click',scoreUpdate);
-goatIcon2.addEventListener('click',scoreUpdate1);
-cowIcon.addEventListener('click',scoreUpdate2);
-oxIcon.addEventListener('click',scoreUpdate3);
-camelIcon.addEventListener('click',scoreUpdate4);
 
 
 function scoreUpdate(){
@@ -149,6 +154,23 @@ function scoreUpdate4(){
     setTimeout( function(){
         camelIcon.className = camelIcon.className.replace('effect_dylan', '');
     },505);
+}
+
+
+
+
+//reset
+var restart = document.querySelector('.restart img');
+restart.addEventListener("click",playAgain);
+function playAgain(){
+    result.innerText = 10;
+    scoreVal.innerText = 0;
+    counter = 10;
+    score = 0<10? "0"+score : score;
+    interval = 0;
+    slide2.className = slide1.className.replace('show', 'hidden');
+    slide1.className = slide2.className.replace('hidden', 'show');
+    checkIT();
 }
 
 
